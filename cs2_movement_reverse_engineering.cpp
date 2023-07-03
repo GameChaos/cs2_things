b3b2e825c8e06e40a0f4c83e3a3c92984761eece
GameChaos <franzkits@gmail.com>
2023-07-03 20:45:10 +0300

    Add new things to CGlobalVarsBase

--- cs2_movement_reverse_engineering.cpp
+++ cs2_movement_reverse_engineering.cpp
@@ -5160,70 +5160,24 @@ public:
 	Vector m_outWishVel; // 0xd4
 };
 
+// Size: 0x78
+// TODO: rename to CGlobalVars, or inherit CGlobalVars from this
 class CGlobalVarsBase
 {
 public:
-	/*
-	// Absolute time (per frame still - Use Plat_FloatTime() for a high precision real time 
-	//  perf clock, but not that it doesn't obey host_timescale/host_framerate)
-	float			realtime;
-	// Absolute frame counter - continues to increase even if game is paused
-	int				framecount;
-	// Non-paused frametime
-	float			absoluteframetime;
-	float			absoluteframestarttimestddev;
-
-	// Current time 
-	//
-	// On the client, this (along with tickcount) takes a different meaning based on what
-	// piece of code you're in:
-	// 
-	//   - While receiving network packets (like in PreDataUpdate/PostDataUpdate and proxies),
-	//     this is set to the SERVER TICKCOUNT for that packet. There is no interval between
-	//     the server ticks.
-	//     [server_current_Tick * tick_interval]
-	//
-	//   - While rendering, this is the exact client clock 
-	//     [client_current_tick * tick_interval + interpolation_amount]
-	//
-	//   - During prediction, this is based on the client's current tick:
-	//     [client_current_tick * tick_interval]
-	*/
-	// Time spent on last server or client frame (has nothing to do with think intervals)
 	uint8_t unknown0[0x10];
 	int maxClients;
-	uint8_t unknown1[0x10];
-	int tickcount;
-	float frametime;
-	float curtime;
-	uint8_t unknown2[0x3C];
-	/*
-	// current maxplayers setting
-	int				maxClients;
-
-	// Simulation ticks - does not increase when game is paused
-	int				tickcount;
-
-	// Simulation tick interval
-	float			interval_per_tick;
-
-	// interpolation amount ( client-only ) based on fraction of next tick which has elapsed
-	float			interpolation_amount;
-	int				simTicksThisFrame;
-
-	int				network_protocol;
-
-	// current saverestore data
-	void *pSaveData; // CSaveRestoreData *
-
-	// Set to true in client code.
-	bool			m_bClient;
-	// true if we are a remote clinet (needs prediction & interpolation - server not on this machine) as opposed to split-screen or local
-	bool			m_bRemoteClient;
-	// 100 (i.e., tickcount is rounded down to this base and then the "delta" from this base is networked
-	int				nTimestampNetworkingBase;   
-	// 32 (entindex() % nTimestampRandomizeWindow ) is subtracted from gpGlobals->tickcount to set the networking basis, prevents
-	//  all of the entities from forcing a new PackedEntity on the same tick (i.e., prevents them from getting lockstepped on this)
-	int				nTimestampRandomizeWindow;  
-	*/
+	uint8_t unknown1[0xc];
+	uint64_t (*MaybeCheckCurtime)(int isInSimulation);
+	float frametime; // 0x28
+	float curtime; // 0x2C
+	uint8_t unknown2[0x10];
+	int tickcount; // 0x40
+	float interval_per_tick; // 0x44
+	
+	// now CGlobalVars maybe
+	char *mapname; // 0x48
+	uint8_t unknown3[0xc];
+	bool mp_teamplay; // 0x5c
+	uint8_t unknown4[0x1b];
 };
